5c987b6a33575e8d361492b321de53a3c0d9689c
Zach Ras <zachras@live.com>
2014-04-01 23:45:26 -0500

    Added cardlist and did some autocomplete

--- TyrantSim/load/js/TyrantSim.js
+++ TyrantSim/load/js/TyrantSim.js
@@ -1,17 +1,27 @@
 define([
     // The dojo/dom module is required by this module, so it goes
     // in this list of dependencies.
-    'dojo/dom'
-], function(dom){
+    'dojo/dom',"dojo/store/Memory", "dijit/form/FilteringSelect"
+], function(dom, Memory, FilteringSelect){
     // Once all modules in the dependency list have loaded, this
     // function is called to define the demo/myModule module.
     //
     // The dojo/dom module is passed as the first argument to this
     // function; additional modules in the dependency list would be
     // passed in as subsequent arguments.
+
+    stateStore = new Memory({data: Cardlist});
+
+    var filteringSelect = new FilteringSelect({
+        id: "input1",
+        name: "card",
+        value: "Aegis",
+        store: stateStore,
+        searchAttr: "CardName"
+    }, "input1");
  
     var oldText = {};
- 
+
     // This returned object becomes the defined value of this module
     return {
         setText: function (id, text) {
@@ -26,4 +36,19 @@ define([
             delete oldText[id];
         }
     };
+});
+
+
+require([
+    , "dojo/domReady!"
+], function(){
+    
+
+    var filteringSelect = new FilteringSelect({
+        id: "stateSelect",
+        name: "state",
+        value: "CA",
+        store: stateStore,
+        searchAttr: "name"
+    }, "stateSelect");
 });
