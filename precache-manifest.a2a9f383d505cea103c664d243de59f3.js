bbd7bc3d4dd5d4602f7c8c15c817aaa70125c0bb
Zach Rasavanh <zrasavanh@tesla.com>
2019-12-21 17:13:55 -0600

    mobile view 2.0

---
+++ precache-manifest.a2a9f383d505cea103c664d243de59f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472246630379f0cc0c782ee3b9c2c140",
+    "url": "/index.html"
+  },
+  {
+    "revision": "949c63e1e7adb30f3d6b",
+    "url": "/static/css/main.324ab680.chunk.css"
+  },
+  {
+    "revision": "4bbdc5a326ebe48dc45a",
+    "url": "/static/js/2.f84efce4.chunk.js"
+  },
+  {
+    "revision": "949c63e1e7adb30f3d6b",
+    "url": "/static/js/main.82a53ef2.chunk.js"
+  },
+  {
+    "revision": "1cad6bd13b3ef2aa479f",
+    "url": "/static/js/runtime-main.8efa6319.js"
+  }
+]);
